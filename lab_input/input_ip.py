5351b7d4fce33b727532753f3dce18574567c5a0
Nick <Nick.Mahoney04@gmail.com>
2018-05-21 14:53:04 -0700

    Added comments

--- lab_input/input_ip.py
+++ lab_input/input_ip.py
@@ -1,10 +1,10 @@
 #!/usr/local/bin python3
 
-user_input = input("Please enter an IPv4 address: ")
+user_input = input("Please enter an IPv4 address: ") #Use the input functoion to gather information from stdin by the user and add it to the variable callled user_input
 
-print("You told me the IPv4 address is: " + user_input)
+print("You told me the IPv4 address is: " + user_input) #concatenates the message in qoutes along with the user supplied input and prints to stdout
 
 
-vendor_name = input("Enter the vendor name of your computer: ")
+vendor_name = input("Enter the vendor name of your computer: ")#Same as above with user_input variable
 
-print("You told me the vendor name is: " + vendor_name)
+print("You told me the vendor name is: " + vendor_name) #same as the print statement on line 5
