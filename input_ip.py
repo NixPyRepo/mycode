ac8d6cb6b34f4ccfd3864de76cee2f5d45d3908d
Nick <Nick.Mahoney04@gmail.com>
2018-05-21 14:09:20 -0700

    Testing edits

---
+++ input_ip.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin python3
+
+user_input = input("Please enter an IPv4 address: ")
+
+print("You told me the IPv4 address is: " + user_input)
+
+
+vendor_name = input("Enter the vendor name of your computer: ")
+
+print("You told me the vendore name is: " + vendor_name)
