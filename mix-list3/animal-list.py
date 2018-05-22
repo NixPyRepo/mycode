1bd1ee0ea6e708eed90436ff9348b4cabb696159
Nick <Nick.Mahoney04@gmail.com>
2018-05-22 12:03:15 -0700

    add comments

--- mix-list3/animal-list.py
+++ mix-list3/animal-list.py
@@ -3,7 +3,8 @@
 #Description: Print all items in a list individually, but on the same line
 
 
-animals = ["cat", "dog", "fox", "cow", "rat"]
+animals = ["cat", "dog", "fox", "cow", "rat"] #assign items to animals list
 
+##loop over the animal list and print each item on the same line
 for i in animals:
     print(i, end=" ")
