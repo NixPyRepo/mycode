037c24139d64fc1fbe9b619fa1397e7920162c00
Nick <Nick.Mahoney04@gmail.com>
2018-05-22 08:33:25 -0700

    Fixed comments

--- day2/orange.py
+++ day2/orange.py
@@ -1,13 +1,15 @@
 #!/usr/local/bin python3 #shebang is used to tell which interpreter needs to be used. In this case python 3
+#Written by Nick Mahoney
 
-
-##Simple scripty to print the word orange on a single line with 6 print functions
+##Simple script to print the word orange on a single line with 6 print functions
 
 word = "orange" #add the word orange to the variable word as a string
 
-print(str(word[0]), end="") #Starting at the first index of the array print the first letter as a str
-print(str(word[1]), end="") #print the second index of the array
-print(str(word[2]), end="") #print the third index of the array
-print(str(word[3]), end="") #print the fourth index of the array
-print(str(word[4]), end="") #print the fifth index of the array
-print(str(word[5]), end="") #print the sixth index of the array
+
+##Print the word orange by calling indicies in the array "word"
+print(str(word[0]), end="")
+print(str(word[1]), end="")
+print(str(word[2]), end="")
+print(str(word[3]), end="")
+print(str(word[4]), end="")
+print(str(word[5]), end="")
