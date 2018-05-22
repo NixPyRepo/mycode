d3697fb74f5fd7ad6d5bcd96ca68f7fd18b2e7b3
Nick <Nick.Mahoney04@gmail.com>
2018-05-22 08:40:13 -0700

    Script to gather user supplied info

---
+++ day2/ipask.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin python3
+#Written by Nick Mahoney
+
+##Python script to collect IP address and MAC address from user supplied input
+
+
+ip_add = input("Please enter your IP address: ") #Prompt users for their IP address
+
+mac_add = input("Please enter your MAC Address: ") #Prompt users for their MAC address
+
+print("Your said your IP address is " + ip_add + " and your MAC address is " + mac_add) #print a message with the user supplied inputs
