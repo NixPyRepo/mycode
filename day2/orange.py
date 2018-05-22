a5280c5add4bd8e62136003a58c92fc8ff61e2b4
Nick <Nick.Mahoney04@gmail.com>
2018-05-22 08:16:43 -0700

    day 2 morning lab. print the word orange with 6 print functions on a single line

---
+++ day2/orange.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin python3 #shebang is used to tell which interpreter needs to be used. In this case python 3
+
+
+##Simple scripty to print the word orange on a single line with 6 print functions
+
+word = "orange" #add the word orange to the variable word as a string
+
+print(str(word[0]), end="") #Starting at the first index of the array print the first letter as a str
+print(str(word[1]), end="") #print the second index of the array
+print(str(word[2]), end="") #print the third index of the array
+print(str(word[3]), end="") #print the fourth index of the array
+print(str(word[4]), end="") #print the fifth index of the array
+print(str(word[5]), end="") #print the sixth index of the array
