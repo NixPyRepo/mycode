7b74cebe5813f1afe18776a10f70cc7dcd6542cb
Nick <Nick.Mahoney04@gmail.com>
2018-05-23 10:03:40 -0700

    working with dictionaries

--- dict01/dict01.py
+++ dict01/dict01.py
@@ -1,14 +1,29 @@
 #!/usr/local/bin python3
 #Author: Nick Mahoney
-#Description:
+#Description: Working with dictionaries and adding key value pairs
 
 
-## create a dictionary
 switch = {'hostname': 'sw1', 'ip': '10.0.1.1', 'version': '1.2', 'vendor': 'cisco'}
 
 ## display parts of the dictionary
-print(switch['hostname'])
-print(switch['ip'])
+print( switch['hostname'] )
+print( switch['ip'] )
 
 ## request a 'fake' key
-print(switch['lynx'])
+# print( switch['lynx'] )
+
+## request a 'fake' key with .get() method
+print( "First test - .get()" )
+print(switch.get('lynx'))
+
+print( "Second test - .get()" )
+print(switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!"))
+
+print( "Third test - .get()" )
+print(switch.get('version'))
+
+print( "Fourth test - .keys()" )
+print(switch.keys())
+
+print( "Fifth test - .values()" )
+print(switch.values())
