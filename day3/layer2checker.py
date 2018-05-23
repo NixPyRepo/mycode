1d68296fc9b10f086f37747c9f37af37da482436
Nick <Nick.Mahoney04@gmail.com>
2018-05-23 11:46:08 -0700

    replaced "True" w/ condition in while loop

--- day3/layer2checker.py
+++ day3/layer2checker.py
@@ -1,17 +1,21 @@
-#!/usr/local/bin python3
+#!/usr/bin/env python3
 #Author: Nick Mahoney
 #Description: prompts the user for an L2 protocol and loops until a valid L2 protocol is entered
 
-
+#Add L2 protocols and message into a dictionary
 l2Proto = {"eth": "ethernet protocol allowed",
            "fc": "fiber channel NOT allowed",
            "ifb": "infiniband NOT allowed",
            "otn": "optical network allowed"}
+
+#
 user_l2 = ""
-while True:
+while (user_l2 not in l2Proto.keys()):
+
     user_l2 = input("Enter your L2 protocol: ")
-    if user_l2 not in l2Proto.keys():
-        print("no idea what technology that is\n")
-    else:
+
+    if user_l2 in l2Proto.keys():
         print(l2Proto[user_l2])
-        break
+
+    else:
+        print("no idea what technology that is\n")
