d1947bc96287b549824bdecd88ed21e95bba2245
Nick <Nick.Mahoney04@gmail.com>
2018-05-23 11:15:50 -0700

    refactored code to use a dictionary and cleaned up the while and ifs

--- day2/ip_if_tester.py
+++ day2/ip_if_tester.py
@@ -2,9 +2,8 @@
 #Author: Nick Mahoney
 #Description: get an ip address from user and check "if" it matches DNS or Gateway IP
 
-#Set gateway and dns variables for easy change later
-gateway = "10.10.3.1"
-dns_server = "10.20.5.2"
+#Set gateway and dns ps in a dictionary for easy change later
+bad_IPs = {"gateway": "10.10.3.1", "dns_server": "10.20.5.2"}
 
 #Create an empty list to add IPs to
 ip_list = []
@@ -12,28 +11,38 @@ ip_list = []
 #Create the ipTester function to be used later
 def ipTester():
 
-    user_ip = input("\nPlease enter an IP address: ") #Get user supplied IP address
+    #Get user supplied IP address
+    user_ip = input("\nPlease enter an IP address or \"Q\" to quit: ")
 
-    #Check user_ip against gateway and dns_server. if true re-prompts for new ip address
-    if user_ip == gateway:
-        print("\nThis is the Gateway IP address. Please choose another IP address")
-        ipTester()
-    elif user_ip == dns_server:
-        print("\nThis is the DNS server IP address. Please choose another IP address")
-        ipTester()
-    else:#If the user supplied IP does not match GW or dns_server add it to a list
+    #loop over the dictionary to check for invalid ips
+    for key, value in bad_IPs.items():
+
+        #if an invalid IP is enter display error message and prompt for new IP
+        if user_ip == value:
+            print("\nThis is the " + key + " IP address. Choose a different IP address")
+
+            #sets user_ip to different string. This keeps the bad_ips from being added to the list
+            user_ip = "bad_ip"
+
+    #As long as the ip is valid and the quit key is not entered the user supplied data will be added to the list
+    if user_ip != "bad_ip" and user_ip.upper() != "Q":
         ip_list.append(user_ip)
 
+    #Return the data in the user_ip variable to be checked in the while loop
+    return user_ip
 
-user_answer = "" #create an empty variable for user input
+#create an empty variable for user input
+choice = ""
 
-while user_answer != "Q": #Loop will continue to gather IPs, until the user enter "Q"
+#Loop will continue to gather IPs, until the user enters "Q"P
+while choice.upper() != "Q":
 
-    ipTester() #tStart the program by calling the ipTester functions
-    user_answer = input("\nTo quit press \"Q\" or Enter to continue").upper() #Confirm if user want to quit or continue and store it as upper case
+    #Start the program by calling the ipTester functions and add the return value to choice
+    choice = ipTester()
 
 #When finished print the list of ips entered
 print("\nThe IPs in the list are: ")
+
 for i in ip_list:
     print(i)
 
