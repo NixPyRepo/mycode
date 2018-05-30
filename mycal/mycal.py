f861a658dd3e899aa2db67e6f9d2d2198dcc3763
Nick <Nick.Mahoney04@gmail.com>
2018-05-30 14:59:56 -0700

    Added description

--- mycal/mycal.py
+++ mycal/mycal.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
+'''
+Author: Nick Mahoney
+Description:
+    use calendar and datetime to print each month in th year with a loop
+    and find the next leap year
+'''
+
 import calendar
 import datetime
+
+
 #A loop to call each month of the year and print the calendar
 for i in range(1,13):
     lilcal = calendar.month(2018, i)
