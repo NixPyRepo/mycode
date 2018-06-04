b01e0c71e75a2f63d9e774b2857a58470073f8dc
Nick <Nick.Mahoney04@gmail.com>
2018-06-04 14:33:03 -0700

    Setup paramiko

---
+++ pmiko/pmiko.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import paramiko
+from sys import argv
+
+
+#Set up the ssh client
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+#Establish the connection
+client.connect('localhost', username=argv[1], password=argv[2])
+
+
+#Accept argument
+cmd = argv[3]
+
+
+def run_cmd(command):
+
+    stdin, stdout, stderr =  client.exec_command(command)
+
+    if not stderr:
+        return stdout
+    else:
+        return stderr
+
+
+result = run_cmd(cmd)
+
+print('result ' + cmd)
+
+for line in result:
+    print(' ' + line.strip('\n'))
+
+
